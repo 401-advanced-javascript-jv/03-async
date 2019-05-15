e117411da63dd3d67650fb3d51bf4cb947db0655
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-05-15 16:31:45 -0700

    edit-file module complete

--- edit-file.js
+++ edit-file.js
@@ -3,20 +3,40 @@
 const fs = require('fs');
 
 // extract the third argument, or return an empty array
-const filename = process.args.slice(2, 3);
+const filename = process.argv.slice(2, 3)[0];
 
 // Empty array means no filename given, so error out
-if ( !Array.isArray(files) || files.length ) {
+if ( !filename ) {
   throw new Error('Missing or invalid argument');
 }
 
-// flags for open:
+// flags for fs.open:
 // a+ = Opens file for read and append, creates file if not exists
 //      Should not be used for positional writing
-// r+ = Opens file for read and write (positional specification possible)
+// r+ = Opens file for read and write (possible to specify file position)
 //      Creates file if not exists
 // w+ = Opens file for read and write, creates file if not exists
 //      truncates file if exists
-fs.open(filename, 'a+', (err, fileData) => {
+fs.readFile(filename, (err, data) => {
+  
+  if (err) { throw err; }
+
+  // Make a random string and convert it to a Buffer
+  // Buffers are what we use to write to a file.
+  let randInsert = (Math.random() * 10000).toString() + '\n';
+
+  console.log(data.toString());
+
+  fs.writeFile(filename, randInsert, {flag: 'a+'}, (err) => {
+    if (err) { throw err; }
+    
+    fs.readFile(filename, (err, data) => {
+      if (err) { throw err; }
+      
+      console.log(data.toString());
+
+    });
+
+  });
 
 });
